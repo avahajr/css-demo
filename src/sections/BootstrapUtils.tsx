23d5b03775c60e1367f19b34d6c4e7538c382f98
Ava Hajratwala <ahajratwala@mtb.com>
2026-03-11 09:08:39 -0400

    colors

--- src/sections/BootstrapUtils.tsx
+++ src/sections/BootstrapUtils.tsx
@@ -28,6 +28,26 @@ export default function BootstrapUtils() {
                 <div className={'col-5 tw:text-green-700/80 tw:outlined-col'}>col-5</div>
             </div>
         </Example>
-        </section>);
+        <h4>Colors</h4>
+        <Example caption={"In Uportal, Central Camp has defined a custom boostrap theme that remaps these colors according to the M&T brand."}>
+            <div className={'d-flex tw:justify-around'}>
+                <div className="text-primary">primary</div>
+                <div className="text-secondary">secondary</div>
+                <div className="text-success">success</div>
+                <div className="text-danger">danger</div>
+                <div className="text-warning">warning</div>
+                <div className="text-info">info</div>
+                <div className="text-light bg-dark">light</div>
+                <div className="text-dark">dark</div>
+            </div>
+        </Example>
+        <p>You can apply these colors using the utility classes (either <code>text-{"<color>"}</code> or <code>bg-{"<color>"}</code>).</p>
+
+        <h5>Using themed color variables directly</h5>
+        <p>If you need to set the color of something <i>other</i> than text or background (i.e., border color), use the color variables.</p>
+        <div className={'container'}>
+            <img alt='bootstrap colors in css' src={'./colors.png'}/>
+        </div>
+    </section>);
 }
 
